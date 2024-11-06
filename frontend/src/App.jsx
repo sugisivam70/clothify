cec96870e3d06ac6318c78f5c42bc0219aafe97d
sugisivam70 <sugisivam.work@gmail.com>
2024-11-06 15:00:59 +0530

    hi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
-
-
+import {Route, Routes} from 'react-router-dom'
 const App = () => {
   return (
-    <div>
-      
+    <div className=" px-4 sm:[5vm] md:px-[7vw] lg:[px-9vw]">
+      <Routes>
+         <Route path='/home' element={<home/>}  />
+         <Route path='/collection' element={<collection/>} />
+         <Route path='/about' element={<about/>} />
+         <Route path='/contact' element={<contact/>} />
+         <Route path='/product/:productid' element={<product/>} />
+         <Route path='/cart' element={<cart/>} />
+         <Route path='/login' element={<login/>} />
+         <Route path='/placeorder' element={<placeorder/>} />
+         <Route path='/oder' element={<oder/>} />
+
+
+      </Routes>
     </div>
   )
 }
